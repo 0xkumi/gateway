dd6ff988cc8fe6d71bafc78dff1a43870d409ce1
admin <dung.q@autonomous.nyc>
2018-04-06 18:11:44 +0700

    test btc send

--- test/btc_wallet.js
+++ test/btc_wallet.js
@@ -4,13 +4,17 @@ var bs58check = require('bs58check')
 var client 
 //'' : mmvhm8KssEvxErP8FRCvUJHUnQWRVCoiaL
 //'four':  mmh2moWwkYdQN7ka9BZmD3ufmm8aFfs7d9 cNQB3968JmURv41YVcMbfBP9Wk4HT5V3qmuX8vVSZjyAvABPK6Fd ef186be8246dc0afe86a583acc0992ee81f9e79f359470724518e0416aeae5acbd01
+
+//transaction: 50c66704d5254267de673027f256aa09a1cbe521c10d4d70925060fb2b8ec99c
 async function start(){
     client = await new Client({ host: "173.212.209.107", port: 18443, username: "test", password: "pass" })
+    // await client.generate(1)
     // console.log(await client.listAccounts())
+    console.log(await client.getTransaction("50c66704d5254267de673027f256aa09a1cbe521c10d4d70925060fb2b8ec99c"))
     // console.log(await client.importPrivKey(createPrivateKeyInWIF(),'four'))
     // console.log(await client.listAccounts())
     // console.log(await client.getAddressesByAccount("third"))
-    sendFrom("mmvhm8KssEvxErP8FRCvUJHUnQWRVCoiaL", "mmh2moWwkYdQN7ka9BZmD3ufmm8aFfs7d9", 1, false)
+    // estimateFee("mmvhm8KssEvxErP8FRCvUJHUnQWRVCoiaL", { "mmh2moWwkYdQN7ka9BZmD3ufmm8aFfs7d9": 1, "mmvhm8KssEvxErP8FRCvUJHUnQWRVCoiaL": 48.9999})
 }
 start()
 
@@ -23,34 +27,51 @@ function createPrivateKeyInWIF(network = "testnet"){
 }
 
 //rootAddress: in case value is greater than fromAddress balance, using rootAddress to transfer
-async function sendFrom(fromAddress, toAddress, value, feeType, rootAddress) {
+async function sendFrom(fromAddress, toAddressArr, value, txFee) {
     var utxo = await client.listUnspent(1, null, [fromAddress])
     var balance = await addressBalance(fromAddress)
-
-    if (balance < value) {
-        if (!rootAddress) {
-            console.log("Insufficent Balance")
-            throw new Error("Insufficent Balance")
-        }
-        var root_utxo = await client.listUnspent(1, null, [rootAddress])
-        utxo = root_utxo ? utxo.concat(root_utxo) : utxo
+    if (balance < value + fee) {
+        console.log("Insufficent Balance")
+        throw new Error("Insufficent Balance")
     }
     
+}
 
-
-    console.log(utxo)
+async function estimateFee(fromAddress, toObj, privateKey){
+    var sendTotal = 0;
+    for (var k in toObj){
+        sendTotal += toObj[k]
+    }
+    console.log(fromAddress, sendTotal)
+    var utxo = await findUtxoForTransfer(fromAddress, sendTotal)
+    if (!utxo) {
+        console.log("Insufficent Balance")
+        throw new Error("Insufficent Balance")
+    }
     
+    const vin = utxo.map((x)=>{
+        return {
+            txid: x.txid,
+            vout: x.vout
+        }
+    })
+    console.log(vin, toObj)
+    var rawTx = await client.createRawTransaction(vin, toObj)
+    console.log(rawTx)
+    var signTx = await client.signRawTransaction(rawTx, null, privateKey ? [privateKey] : null)
+    console.log(signTx)
+    console.log(await client.sendRawTransaction(signTx.hex, false))
 }
 
-function constructTransaction(vin, vout){
-
-}
-
-async function addressBalance(address){
+async function findUtxoForTransfer(address, value){
     var utxo = await client.listUnspent(1, null, [address])
     let sum = 0;
+    let result = []
     for (var i in utxo){
         sum += utxo[i].amount
+        result.push(utxo[i])
+        if (sum >= value) break;
     }
-    return sum
+    if (sum < value) return null;
+    return result
 }
